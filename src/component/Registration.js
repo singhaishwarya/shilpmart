5890a0a2ec39d953998ce900c0867b0a6edeaf66
Ram Chandra <chaurasia4u@gmail.com>
2021-06-03 17:29:54 +0530

    update css and product page layout

--- src/component/Registration.js
+++ src/component/Registration.js
@@ -152,11 +152,8 @@ export default class Registration extends React.Component {
 
           <div className="col-md-6 col-12 mb-5">
             <h4 className="mb-4">Registration</h4>
-            {/* <form action="#" className="login-card shadow p-4 border rounded" onSubmit={(e) => this.handleSignUp(e)}>
-              <div className="row"> */}
-
-            <Form onSubmit={this.handleSignUp} ref={(c) => { this.form = c; }}>
-            <div className="form-group row">
+             <Form className="login-card" onSubmit={this.handleSignUp} ref={(c) => { this.form = c; }}>
+              <div className="form-group row">
     <label for="staticEmail" className="col-sm-3 col-form-label">First Name<span>*</span></label>
     <div className="col-sm-9">
     <Input type="text" className="form-control" name="first_name" value={fields.first_name} onChange={this.handleChange.bind(this, "first_name")} validations={[required, lt50]}/>
@@ -166,8 +163,7 @@ export default class Registration extends React.Component {
   <div className="form-group row">
     <label for="staticEmail" className="col-sm-3 col-form-label">Last Name<span>*</span></label>
     <div className="col-sm-9">
-    <Input
-                      type="text"
+    <Input type="text"
                       className="form-control"
                       name="last_name"
                       value={fields.last_name}
@@ -184,78 +180,33 @@ export default class Registration extends React.Component {
     <Input
                       type="text"
                       className="form-control"
-                      name="last_name"
-                      value={fields.last_name}
-                      onChange={this.handleChange.bind(this, "last_name")}
-                      validations={[required, lt50]}
-                    />
-    </div>
-  </div>
-               <div className="row">
-                {/* <div className="col-lg-6 col-12">
-                  <div className="form-group"><label htmlFor="fname">First Name<span>*</span></label>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      name="first_name"
-                      value={fields.first_name}
-                      onChange={this.handleChange.bind(this, "first_name")}
-                      validations={[required, lt50]}
-                    />
-                    {/* <input type="text" className="form-control" value={fields.first_name || ''} onChange={this.handleChange.bind(this, "first_name")} /> 
-                  </div>
-                </div> */}
-                <div className="col-lg-6 col-12">
-                  <div className="form-group"><label htmlFor="lname">Last Name<span>*</span></label>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      name="last_name"
-                      value={fields.last_name}
-                      onChange={this.handleChange.bind(this, "last_name")}
-                      validations={[required, lt50]}
-                    />
-                    {/* <input type="text" onChange={this.handleChange.bind(this, "last_name")} value={fields.last_name || ''} className="form-control" /> */}
-
-                  </div>
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-lg-6 col-12">
-                  <div className="form-group"><label htmlFor="email">Email<span>*</span></label>
-                    <Input
-                      type="text"
-                      className="form-control"
                       name="email"
                       value={fields.email}
                       onChange={this.handleChange.bind(this, "email")}
                       validations={[required, email, lt50]}
                     />
-                    {/* <input type="email" onChange={this.handleChange.bind(this, "email")} value={fields.email || ''} className="form-control" /> */}
+    </div>
+  </div>
 
-                  </div>
-                </div>
-                <div className="col-lg-6 col-12">
-                  <div className="form-group"><label htmlFor="mNo">Mobile No.<span>*</span></label>
-                    <Input
-                      type="number"
+  <div className="form-group row">
+    <label for="staticEmail" className="col-sm-3 col-form-label">Mobile No.<span>*</span></label>
+    <div className="col-sm-9">
+    <Input
+                      type="tel"
                       className="form-control"
                       name="mobile"
                       value={fields.mobile}
                       onChange={this.handleChange.bind(this, "mobile")}
                       validations={[required, mobile]}
                     />
-                    {/* <input type="number" onChange={this.handleChange.bind(this, "mobile")} value={fields.mobile || ''} className="form-control" /> */}
+    </div>
+  </div>
 
-                  </div>
-                </div>
-              </div>
 
-              <div className="row">
-                <div className="col-lg-6 col-12">
-                  <div className="form-group"><label htmlFor="pass">Password<span>*</span></label>
-                    <Input
+  <div className="form-group row">
+    <label for="staticEmail" className="col-sm-3 col-form-label">Password<span>*</span></label>
+    <div className="col-sm-9">
+    <Input
                       type="password"
                       className="form-control"
                       name="password"
@@ -263,13 +214,14 @@ export default class Registration extends React.Component {
                       onChange={this.handleChange.bind(this, "password")}
                       validations={[required, isValidpassword]}
                     />
-                    {/* <input type="password" onChange={this.handleChange.bind(this, "password")} value={fields.password || ''} className="form-control" /> */}
+    </div>
+  </div>
 
-                  </div>
-                </div>
-                <div className="col-lg-6 col-12">
-                  <div className="form-group"><label htmlFor="cpass">Confirm Password<span>*</span></label>
-                    <Input
+
+  <div className="form-group row">
+    <label for="staticEmail" className="col-sm-3 col-form-label">Confirm Password<span>*</span></label>
+    <div className="col-sm-9">
+    <Input
                       type="password"
                       className="form-control"
                       name="c_password"
@@ -277,14 +229,10 @@ export default class Registration extends React.Component {
                       onChange={this.handleChange.bind(this, "c_password")}
                       validations={[required, isValidpassword]}
                     />
-                    {/* <input type="password" onChange={this.handleChange.bind(this, "c_password")} value={fields.c_password || ''} className="form-control" /> */}
-
-                  </div>
-                </div>
-              </div>
-              <fieldset>
+    </div>
+  </div>      
                 <Button className="btn login-btn" value="Submit"  >Register</Button>
-              </fieldset>
+              
             </Form>
           </div>
         </div>
