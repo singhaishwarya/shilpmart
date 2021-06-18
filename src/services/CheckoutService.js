9953d359d0334d2ed38d415d12b8720c15b12587
singhaishwarya <aishwarya@expansionjs.com>
2021-06-18 16:08:24 +0530

    service updated

--- src/services/CheckoutService.js
+++ src/services/CheckoutService.js
@@ -46,6 +46,21 @@ export default class Checkout extends Component {
       console.log(`FETCH GET ERROR`, response);
     }
   }
+
+  static txnStatus = async (data) => {
+    try {
+      config.url = baseUrl + 'order-validate';
+      config.method = 'post';
+      config.data = data;
+      config.params = '';
+      const response = await axios(config);
+      return response.data ? response.data : [];
+    } catch (error) {
+      const { response } = error;
+      if (!response) return;
+      console.log(`FETCH GET ERROR`, response);
+    }
+  }
 }
 
 
